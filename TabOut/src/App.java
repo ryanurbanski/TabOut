14731689501966da753038f955c625488a9f7d50
Ryan Urbanski <ryanurbanski9@gmail.com>
2019-10-29 20:09:00 -0400

    First time committing on this repository.  Added one file with main method

---
+++ TabOut/src/App.java
@@ -0,0 +1,18 @@
+/*
+ * Project to transfer existing code to
+ */
+
+/**
+ * 
+ * @author ryan
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // First draft of new combined master project file
+    }
+    
+}
